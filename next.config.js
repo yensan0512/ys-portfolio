65f9f04a26ecbfb11778db048c43610d41fb015d
jiajinho <hojiajin7@gmail.com>
2023-02-19 14:51:07 +0800

    setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: {
+      displayName: true
+    }
+  }
 }
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+`;
+
+export default function Home() {
+  return (
+    <Wrapper>
+      hi
+    </Wrapper>
+  )
+}

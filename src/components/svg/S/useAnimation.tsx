9a868778843d89981501a647a66f20790dbb5e4d
yensan0512 <yensan1254@gmail.com>
2023-02-19 15:53:04 +0800

    Added S letter with animation

---
+++ src/components/svg/S/useAnimation.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+export default (active: boolean) => {
+    const pathRef = useRef<SVGPathElement>(null);
+
+    useEffect(() => {
+        if (active) {
+            gsap.to(pathRef.current, {
+                duration: 1,
+                ease: "power2.out",
+                strokeDashoffset: 0
+            });
+        }
+    }, [active]);
+
+    return pathRef;
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import Y, { Wrapper as $Y } from '@/components/svg/Y';
 import { useState } from 'react';
+import S, { Wrapper as $S } from '@/components/svg/S';
 
 const Wrapper = styled.main`
 
 
-  ${$Y} {
+  ${$Y},${$S} {
     height: 150px;
   }
 `;
@@ -17,6 +18,7 @@ export default function Home() {
   return (
     <Wrapper>
       <Y animate={animate} />
+      <S animate={animate} />
 
       <button onClick={() => setAnimate(true)}>animate</button>
     </Wrapper>

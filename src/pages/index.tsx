80c5d7c10755263a6c8b3e3a53eae86fa16675c6
yensan0512 <yensan1254@gmail.com>
2023-02-19 15:37:26 +0800

    Added Y letter with animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import styled from 'styled-components';
+import Y, { Wrapper as $Y } from '@/components/svg/Y';
+import { useState } from 'react';
 
 const Wrapper = styled.main`
+
+
+  ${$Y} {
+    height: 150px;
+  }
 `;
 
 export default function Home() {
+
+  const [animate, setAnimate] = useState(false);
+
   return (
     <Wrapper>
-      hi
+      <Y animate={animate} />
+
+      <button onClick={() => setAnimate(true)}>animate</button>
     </Wrapper>
   )
 }
